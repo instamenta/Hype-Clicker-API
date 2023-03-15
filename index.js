dfdfdd1bc2c6c2ee3a1ebd6b2530fb172c95b946
instamenta <98179343+instamenta@users.noreply.github.com>
2023-03-15 17:16:02 +0200

    mongodb atlas cluster configuration

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ const app = require('./app')
 
 dotenv.config();
 const port = process.env.PORT || 5000
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://janoopsi:janoopsi9999@clickercluster.ltycehn.mongodb.net/?retryWrites=true&w=majority'
+const MONGODB_URL = "mongodb+srv://janoopsi:janoopsi9999@clickercluster.ltycehn.mongodb.net/?retryWrites=true&w=majority"
 
 
-mongoose.connect(MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-  console.log(`MongoDB is connected on ${MONGODB_URL}`)
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB cluster!');
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+    })
+    mongoose.connection.close();
   })
-})
-.catch((error) => {
-  console.error('Error connecting to database:', error)
-})

23bcba8603076bad9c323909ff238ba876dfd5da
instamenta <98179343+instamenta@users.noreply.github.com>
2023-03-15 17:06:27 +0200

    mongodb atlas cluster configuration

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = require('./app')
 
 dotenv.config();
 const port = process.env.PORT || 5000
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://janoopsi:janoopsi9999@clickercluster.ltycehn.mongodb.net/hype-clicker'
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://janoopsi:janoopsi9999@clickercluster.ltycehn.mongodb.net/?retryWrites=true&w=majority'
 
 
 mongoose.connect(MONGODB_URL, {

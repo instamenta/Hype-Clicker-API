0684b538dd3f4d1ec9f802ab6a1982f1a253875e
instamenta <98179343+instamenta@users.noreply.github.com>
2023-03-15 06:56:15 +0200

    added morgan to app.js

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/auth.routes');
-const userRoutes = require('./routes/user.routes');
-const nftRoutes = require('./routes/nft.routes');
-const errorMiddleware = require('./middlewares/error.middleware');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const store = require('./index').store;
+const express = require('express')
+const cors = require('cors')
+const session = require('express-session')
+const passport = require('passport')
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/auth.routes')
+const userRoutes = require('./routes/user.routes')
+const nftRoutes = require('./routes/nft.routes')
+const errorMiddleware = require('./middlewares/error.middleware')
+const MongoDBStore = require('connect-mongodb-session')(session)
+const store = require('./index').store
 
-const app = express();
+const app = express()
+
+app.use(morgan('common', {
+  skip: function (req, res) {
+    
+    return res.statusCode < 400 // don't log successful responses
+  }
+}));
 
 app.use(cors());
 app.use(bodyParser.json());

026182b883339497589d2e1e04fce1b613cccc7f
instamenta <98179343+instamenta@users.noreply.github.com>
2023-03-15 02:53:05 +0200

    routes updated and validaors+ validator middlewares added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user.controller');
+const { validateUserInput, validateUser } = require('../utils/validation.utils');
 
 // Get all users with optional query filters and pagination
 router.get('/', UserController.getAllUsers);
@@ -9,12 +10,15 @@ router.get('/', UserController.getAllUsers);
 router.get('/:id', UserController.getOneUser);
 
 // Update a user's data
-router.patch('/:id', UserController.editUserData);
+router.put('/:id', validateUserInput(), validateUser, UserController.updateUser);
 
 // Add or remove money from a user's account
 router.patch('/:id/money', UserController.modifyUserMoney);
 
 // Change a user's profile picture
 router.patch('/:id/profile-picture', UserController.changeProfilePicture);
+
+// Delete a user
+router.delete('/:id', UserController.deleteUser);
 
 module.exports = router;
